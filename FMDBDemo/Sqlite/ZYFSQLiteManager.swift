a3d753c146c69445d65d2f07b0196e5a7134c90d
张亚峰 <1240334911@qq.com>
2018-09-02 19:55:56 +0800

    创建数据库，串行队列，同步执行

--- FMDBDemo/Sqlite/ZYFSQLiteManager.swift
+++ FMDBDemo/Sqlite/ZYFSQLiteManager.swift
@@ -31,12 +31,36 @@ class ZYFSQLiteManager {
         print("数据库的路径" + path)
         //创建数据库队列,同时‘创建或者打开’数据库
         queue = FMDatabaseQueue(path: path)
-        
+        //打开数据库
+        createTable()
     }
     
 }
 
-
+// MARK: - 创建数据表以及其他私有方法
+private extension ZYFSQLiteManager {
+    
+    /// 创建数据表
+    func createTable()  {
+        guard let path = Bundle.main.path(forResource: "status.sql", ofType: nil),
+            let sql = try? String(contentsOfFile: path)
+        else {
+            return
+        }
+        print(sql)
+        //2.执行 sql - FMDB 的内部队列，串行队列，同步执行
+        // 可以保证同一时间，只有一个任务操作数据库，从而保证数据库的读写安全!
+        // 在执行增删改查的时候，一定不要使用 statements 方法，否则有可能会被注入
+        queue.inDatabase { (db) in
+            if db.executeStatements(sql) == true {
+                print("创表成功")
+            } else {
+                print("创表失败")
+            }
+        }
+        print("over")
+    }
+}
 
 
 
