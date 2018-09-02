7bbd2ff064d3318c350a0b50825962ccf07874e4
张亚峰 <1240334911@qq.com>
2018-09-02 20:21:43 +0800

    准备新增或者修改微博函数，确定函数的参数

--- FMDBDemo/Sqlite/ZYFSQLiteManager.swift
+++ FMDBDemo/Sqlite/ZYFSQLiteManager.swift
@@ -13,6 +13,8 @@ import FMDB
     1.数据库的本质上是保存在沙盒中的一个文件，首先需要创建并且打开数据库
     2.创建数据表
     3.增删改查
+    提示：数据库开发，程序代码几乎是一致的，区别在 SQL
+    开发数据库功能的时候，首先一定要在 navicat 中测试 SQL 的正确性
  **/
 class ZYFSQLiteManager {
     
@@ -37,6 +39,28 @@ class ZYFSQLiteManager {
     
 }
 
+// MARK: - 微博数据操作
+extension ZYFSQLiteManager {
+    //新增或者修改微博数据，微博数据在刷新的时候，可能会出现重叠
+    /**
+     思考:从网络加载结束后，返回的是微博的‘字典数组’，每一个字典对应一个完整的微博记录
+     - 完整的微博记录中，包含微博的代号
+     - 微博记录中，没有‘当前登录的用户代号’
+     **/
+    func updateStatus(userId: String, array: [[String : Any]])  {
+        //1.准备 sql
+        /**
+         statusId:要保存的微博代号
+         userId:当前登录用户的 id
+         status:完整微博字典的 json 二进制数据
+         **/
+        let sql = "INSERT OR REPLACE INTO T_Status (statusId, userId, status) VALUES (?, ?, ?);"
+        //2.执行sql
+        
+    }
+    
+    
+}
 // MARK: - 创建数据表以及其他私有方法
 private extension ZYFSQLiteManager {
     
